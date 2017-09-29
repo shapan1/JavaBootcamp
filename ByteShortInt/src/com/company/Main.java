c32e6bffa074cc5c7a07d07174b9f0f1736150ae
Yu Zhou <yuzhou@internal.synopsys.com>
2017-09-29 13:43:15 -0700

    min max of integer val

---
+++ ByteShortInt/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        int maxVal = Integer.MAX_VALUE;
+        int minVal = Integer.MIN_VALUE;
+        System.out.println("Maximum Value for Integer in Java: " + maxVal);
+        System.out.println("Maximum Value for Integer in Java: " + minVal);
+        System.out.println("Their sum is : " + (maxVal + minVal));
+    }
+}
