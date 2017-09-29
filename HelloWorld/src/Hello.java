095610c9d18f06c7b91b2bd69f221da3f53e053a
Yu Zhou <yuzhou@internal.synopsys.com>
2017-09-29 13:30:43 -0700

    Helloworld

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+//sout = System.out.println();
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Why the hell does people always do Hello World???");
+        double rate = 0.01;
+        int iq = 1000;
+        double chance = rate * iq;
+        System.out.println("The chance of you getting hired is " + chance + "%");
+    }
+}
