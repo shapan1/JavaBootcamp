6091fd82db3c59fcb20d50ca594684387dd33253
Yu Zhou <yuzhou@internal.synopsys.com>
2017-09-29 16:10:53 -0700

    casting

--- ByteShortInt/src/com/company/Main.java
+++ ByteShortInt/src/com/company/Main.java
@@ -1,11 +1,38 @@
 package com.company;
 
+//Type
+
+
 public class Main {
     public static void main(String[] args) {
+//        Min Max
         int maxVal = Integer.MAX_VALUE;
         int minVal = Integer.MIN_VALUE;
         System.out.println("Maximum Value for Integer in Java: " + maxVal);
         System.out.println("Maximum Value for Integer in Java: " + minVal);
         System.out.println("Their sum is : " + (maxVal + minVal));
+
+        // Int/Float/Double
+        int whole = 5 / 3;
+        float f_value = 5f / 3f;
+        double d_value = 5d / 3d;
+
+        System.out.println(whole);
+        System.out.println(f_value);
+        System.out.println(d_value);
+
+        // char/String
+        char myChar = '\u00A9';
+        System.out.println("cool stuff" + myChar);
+
+        String myString = "You are so beautiful to me";
+        System.out.println(myString);
+
+
+        //Casting
+        String num = "3";
+        int res = Integer.parseInt(num) + 3;
+
+        System.out.println(res);
     }
 }
